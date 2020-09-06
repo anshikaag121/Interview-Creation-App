595ffce1b91d6dd48c8f5749bf845325921b0a9f
Anshika Agarwal <anshikaagarwal456@gmail.com>
2020-09-06 11:09:01 +0530

    Deploy SQL

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "test",
-    DB: "interviews",
-    PORT: "3306"
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b0a27ea06831a7",
+    PASSWORD: "4ab3c2b4",
+    DB: "heroku_86b88a34cb039d7"
   };
--- models/db.js
+++ models/db.js
@@ -5,8 +5,7 @@ var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  port: dbConfig.PORT
+  database: dbConfig.DB
 });
 
 module.exports = connection;
